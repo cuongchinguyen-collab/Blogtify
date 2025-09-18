9088ed749faaceb51bbc8bc14e813529aca74cad
chicuong2k3 <nguyenchicuongk21@gmail.com>
2025-09-19 01:32:07 +0700

    update

--- Blogtify/Blogtify/wwwroot/js/theme.js
+++ Blogtify/Blogtify/wwwroot/js/theme.js
@@ -4,6 +4,18 @@
         if (link) {
             link.href = `css/theme-${themeName.toLowerCase()}.css`;
         }
+
+        const lightThemes = ['yeti', 'flatly', 'lumen', 'materia', 'simplex', 'sketchy', 'sandstone'];
+        const darkThemes = ['darkly', 'slate', 'superhero', 'vapor', 'solar'];
+        const codeBlockLink = document.getElementById("code-block-stylesheet");
+        if (codeBlockLink) {
+            if (lightThemes.includes(themeName.toLowerCase())) {
+                codeBlockLink.href = `css/prism-coy-without-shadows.min.css`;
+            }
+            else if (darkThemes.includes(themeName.toLowerCase())) {
+                codeBlockLink.href = `css/prism-vsc-dark-plus.min.css`;
+            }
+        }
     }
 };
 
